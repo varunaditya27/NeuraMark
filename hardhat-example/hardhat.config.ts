f511d1d328c0a4fdfcadfaf9fc42b0442ed19ea8
varunaditya27 <varun.paparajugari@gmail.com>
2025-10-19 10:29:39 +0530

    Add dotenv configuration for environment variables in Hardhat setup

--- hardhat-example/hardhat.config.ts
+++ hardhat-example/hardhat.config.ts
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import type { HardhatUserConfig } from "hardhat/config";
 
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
@@ -33,8 +35,8 @@ const config: HardhatUserConfig = {
     sepolia: {
       type: "http",
       chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      url: process.env.SEPOLIA_RPC_URL!,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY!],
     },
   },
 };
